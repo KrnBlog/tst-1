be050ec87893b47179c9f86868482f41533a2aa0
Developer-karan-projects <karanarora200229@gmail.com>
2022-12-05 21:40:21 +0530

    mongoose req

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express= require("express")
-const app = express() ;
+const express = require("express")
+const app = express();
 const bodypaser = require("body-parser")
 app.use(express.json())
 app.use(bodypaser.urlencoded({
@@ -7,18 +7,32 @@ app.use(bodypaser.urlencoded({
 }))
 
 const PORT = process.env.PORT || 3000
+const mongoose = require("mongoose")
+mongoose.connect('mongodb+srv://karan:karanarora@cluster0.ujsaxn6.mongodb.net/?retryWrites=true&w=majorit', {
+          useNewUrlParser: true,
+          useUnifiedTopology: true
+     },
+     (err) => {
+          if (err) {
+               console.log(err)
+          } else {
+               console.log("connected in database")
+          }
+     }
+)
 
-
-app.listen(PORT ,()=>{console.log("listening at 3000")})
-app.get("/",(req,res)=>{
+app.listen(PORT, () => {
+     console.log("listening at 3000")
+})
+app.get("/", (req, res) => {
      res.sendFile(__dirname + "/index1.html")
      console.log("step -1 root")
 })
-app.get("/form",(req,res)=>{
+app.get("/form", (req, res) => {
      res.sendFile(__dirname + "/form.html")
      console.log("step -2 form page get")
 })
-app.post("/form",(req,res)=>{
+app.post("/form", (req, res) => {
      res.sendFile(__dirname + "/thankyou.html")
      let a = req.body.one;
      console.log(a)
